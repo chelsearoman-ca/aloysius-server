514c5286eeb69b7a6e9581da7a3ad313e9950110
Chelsea Roman <chelsea.ann.roman@gmail.com>
2020-09-27 15:33:02 -0400

    being a nuisance

--- src/config/server.ts
+++ src/config/server.ts
@@ -3,7 +3,7 @@ const app = express();
 const PORT: string | number = process.env.PORT || 5000;
 
 app.get("/hello", (req, res) => {
-  res.json({ message: "Hello World" });
+  res.json({ message: "Hello World 🤘🏽" });
 });
 
 app.listen(PORT, () => console.log(`hosting @${PORT}`));

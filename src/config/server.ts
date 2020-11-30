5dac96f349ea0dd4125a1f0a1e743287de7d956e
Chelsea Roman <chelsea.ann.roman@gmail.com>
2020-11-30 13:31:19 -0500

    ts-postgres doesnt run on ssl

--- src/config/server.ts
+++ src/config/server.ts
@@ -1,3 +1,4 @@
+import { Client } from 'ts-postgres';
 import express from "express";
 const app = express();
 const PORT: string | number = process.env.PORT || 5000;
@@ -6,4 +7,32 @@ app.get("/hello", (req, res) => {
   res.json({ message: "Hello World 🤘🏽 🌎" });
 });
 
+app.get("/mushrooms", async (req, res)=> {
+
+    const client = new Client({"host": "ec2-3-210-23-22.compute-1.amazonaws.com", "database": "d9m67v74a5u802", "user": "vymhwjkzrivzdi", "password": "68c495f35dbbef73e04ed0eafaa332c626b3c52fca87eeb26f823acee6e5accc");
+    console.log('before connect')
+    await client.connect();
+    console.log('connected')
+    try {
+        // Querying the client returns a query result promise
+        // which is also an asynchronous result iterator.
+        const resultIterator = client.query(
+            `SELECT '* ' || $1 || '!' AS message`,
+            ['world']
+        );
+ 
+        for await (const row of resultIterator) {
+            // 'Hello world!'
+            console.log(row.get('message'));
+        }
+    } finally {
+        await client.end();
+    }
+
+})
+
 app.listen(PORT, () => console.log(`hosting @${PORT}`));
+
+ 
+
+ 
